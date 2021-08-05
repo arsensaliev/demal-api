90b0ddeb21dcd91bf25cfcecef6c403aecb8a688
Arsen Saliev <arsensaliev03@gmail.com>
2021-08-05 17:23:20 +0600

    fix: delete ssl field

--- knexfile.ts
+++ knexfile.ts
@@ -9,6 +9,5 @@ module.exports = {
     port: parseInt(process.env.DB_PORT) || 5432,
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'demal',
-    ssl: { rejectUnauthorized: false },
   },
 };
