deba2e247333369000a2d6d14c1d6e8290599aa5
Arsen Saliev <arsensaliev03@gmail.com>
2021-08-05 16:18:59 +0600

    feat: add users table

--- knexfile.ts
+++ knexfile.ts
@@ -5,9 +5,9 @@ module.exports = {
   client: process.env.DB_CONNECTION,
   connection: {
     host: process.env.DB_HOST || '127.0.0.1',
-    user: process.env.DB_USERNAME || 'root',
-    port: parseInt(process.env.DB_PORT) || 3306,
+    user: process.env.DB_USERNAME || 'postgres',
+    port: parseInt(process.env.DB_PORT) || 5432,
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'root',
+    database: process.env.DB_NAME || 'demal',
   },
 };
--- src/config.ts
+++ src/config.ts
@@ -11,8 +11,8 @@ const config = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
     name: process.env.DB_NAME,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    username: process.env.DB_USERNAME || '',
+    password: process.env.DB_PASSWORD || '',
   },
   mail: {
     username: process.env.MAIL_USERNAME,
