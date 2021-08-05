8d6aa17be94b5ec89e452847cb421ed2070dad89
Arsen Saliev <arsensaliev03@gmail.com>
2021-08-05 18:44:42 +0600

    feat: add swagger in project

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Sali Store API')
+    .setDescription('The Sali Store RESTful API documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT || 3000);
 }
