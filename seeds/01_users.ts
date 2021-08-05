15f7f941b17f14c733deb9c4e0bccb993b3ce173
Arsen Saliev <arsensaliev03@gmail.com>
2021-08-05 17:52:16 +0600

    feat: add users seed

---
+++ seeds/01_users.ts
@@ -0,0 +1,15 @@
+import * as Knex from 'knex';
+
+export async function seed(knex: Knex): Promise<void> {
+  await knex('users').del();
+
+  await knex('users').insert([
+    {
+      id: 1,
+      firstName: 'Arsen',
+      lastName: 'Saliev',
+      email: 'arsen@mail.ru',
+      password: '$2a$10$uvngDEcA/Q2b7LA04FtdZeUZxAB/ozJV6dHMlrRWwNmjmHyUlEGwu',
+    },
+  ]);
+}
