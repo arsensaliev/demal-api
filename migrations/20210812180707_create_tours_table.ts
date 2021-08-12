6a342a19288116b8211347971afc91f5989acab5
Arsen Saliev <arsensaliev03@gmail.com>
2021-08-12 19:07:02 +0600

    feat: add tours seeds

--- migrations/20210812180707_create_tours_table.ts
+++ migrations/20210812180707_create_tours_table.ts
@@ -4,7 +4,11 @@ export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('tours', (table) => {
     table.increments('id');
     table.string('title', 255).notNullable();
+    table.string('subTitle', 255).notNullable();
     table.string('place', 255).notNullable();
+    table.integer('price').notNullable().defaultTo(0);
+    table.string('country', 255).notNullable();
+    table.integer('duration').notNullable();
     table.text('description').nullable();
     table.integer('travelersCount').notNullable();
     table.date('startDate').notNullable();
