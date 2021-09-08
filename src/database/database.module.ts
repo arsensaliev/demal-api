861659f764881479b7a71e99cfbc46e2f01d17bb
Arsen Saliev <arsensaliev03@gmail.com>
2021-09-08 21:25:35 +0600

    fix: ssl error

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -10,7 +10,7 @@ interface DatabaseConfiguration {
   name: string;
   username: string;
   password: string;
-  // ssl: { rejectUnauthorized: boolean };
+  ssl: { rejectUnauthorized: boolean };
 }
 
 @Module({})
@@ -30,7 +30,7 @@ export class DatabaseModule {
       name: this.configService.get<string>('DB_NAME') || 'demal',
       username: this.configService.get<string>('DB_USERNAME') || 'postgres',
       password: this.configService.get<string>('DB_PASSWORD') || '',
-      // ssl: { rejectUnauthorized: false },
+      ssl: { rejectUnauthorized: false },
     };
   }
 
@@ -43,9 +43,9 @@ export class DatabaseModule {
         port: configuration.port,
         password: configuration.password,
         database: configuration.name,
-        // ssl: {
-        //   rejectUnauthorized: configuration.ssl.rejectUnauthorized,
-        // },
+        ssl: {
+          rejectUnauthorized: configuration.ssl.rejectUnauthorized,
+        },
       },
     });
 
